b68578a7c1a66fc0b87eaeb1c7c5d20aa79798b5
Ovesh101 <Oveshmulla786@gmail.com>
2024-06-12 19:05:12 +0530

    comment added

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -5,7 +5,17 @@ import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
-
+/*
+Import Required Modules: Prepare the tools and personnel.
+Set Up API Keys: Get the keys for the pass-making machine.
+Define Token Provider Function: Start the process of issuing a pass.
+Get Current User: Check your ID.
+Check for User and Keys: Ensure everything needed is available.
+Create Stream Client: Access the pass-making machine.
+Calculate Expiry and Issued Time: Set the validity period for the pass.
+Create the Token: Generate the temporary access pass.
+Return the Token: Hand over the pass.
+*/
 export const tokenProvider =  async ()=>{
     const user = await currentUser();
 
